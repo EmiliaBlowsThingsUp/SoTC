5a81a655820f6f9e9c83a74250c474fefb1eb054
Emilia <cpierce64897@gmail.com>
2024-03-10 18:37:41 -0700

    Deleted example pages and began the addition of the gold tracking page

---
+++ GoldTracking.cs
@@ -0,0 +1,17 @@
+﻿namespace SoTC
+{
+    public class GoldTracking
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal GoldLastSession { get; set; }
+
+        public string GoldThisSession { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string GetFormattedBasePrice() => GoldLastSession.ToString("0.00");
+    }
+}
