a1f287e0e91538711d152c1076bd5348e3e0d99d
Emilia <cpierce64987@gmail.com>
2024-06-14 07:27:06 -0700

    refactored apache settings and rebuilding

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('register', 'UserController::register');
-$routes->post('usercontroller/store', 'UserController::store');
