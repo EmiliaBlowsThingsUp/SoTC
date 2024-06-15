bf130e447148cf509a995d656a4151ce2668ce8c
Emilia <cpierce64987@gmail.com>
2024-06-15 12:02:29 -0700

    update 1201hrs 6152024
    built home page with nav/footer updates to all page views.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,8 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->setDefaultController('Register');
-$routes->get('/', 'Register::index', ['filter' => 'guestFilter']);
+$routes->setDefaultController('Home');
+$routes->get('/', 'Home::index');
 $routes->get('/register', 'Register::index', ['filter' => 'guestFilter']);
 $routes->post('/register', 'Register::register', ['filter' => 'guestFilter']);
  
---
+++ app/Views/home.php
@@ -0,0 +1,7 @@
+<?=$this->extend("layout")?>
+  
+<?=$this->section("content")?>
+
+<h1> WOOT </h1>
+
+<?=$this->endSection()?>
