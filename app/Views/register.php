6cf3fc5c5b093caf8d841ec5e29a620820400a1d
Emilia <cpierce64987@gmail.com>
2024-06-15 10:10:40 -0700

    functioning forms
    login/registration/dashboard pages now work flawlessly

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,25 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>User Registration</title>
-</head>
-<body>
-    <h1>User Registration</h1>
-    <form action="<?php echo base_url('/register'); ?>" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br>
-
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br>
-
-        <button type="submit">Register</button>
-    </form>
-</body>
-</html>
+<?=$this->extend("layout")?>
+  
+<?=$this->section("content")?>
+  
+<div class="container">
+    <div class="row justify-content-md-center mt-5">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title mb-4">Register</h5>
+                    <form action="<?php echo base_url('/register'); ?>" method="post">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="email" name="email" value="<?= set_value('email') ?>">
+                            <?php if(isset($validation)):?>
+                                <small class="text-danger"><?= $validation->getError('email') ?></small>
+                            <?php endif;?>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                            <?php if(isset($validation)):?>
+                                <small class="text-danger"><?= $validation->getError('password') ?></small>
+                            <?php endif;?>
+                        </div>
+                        <div class="mb-3">
+                            <label for="confirm_password" class="form-label">Confirm Password</label>
+                            <input type="password" class="form-control" id="confirm_password" name="confirm_password">
+                            <?php if(isset($validation)):?>
+                                <small class="text-danger"><?= $validation->getError('confirm_password') ?></small>
+                            <?php endif;?>
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary btn-block">Register Now</button>
+                            <p class="text-center">Have already an account <a href="<?php echo base_url('/login'); ?>">Login here</a><p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+     
+</div>
+  
+<?=$this->endSection()?>
